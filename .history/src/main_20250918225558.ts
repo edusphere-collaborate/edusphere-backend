8bb4d3f1d750288e23dfc9a7bccd33190e48ef1e
drco <derricko271@gmail.com>
2025-09-18 22:57:27 +0000

    update deploy12

--- .history/src/main_20250918225558.ts
+++ .history/src/main_20250918225558.ts
@@ -27,12 +27,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: [
-      '*',
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'http://localhost:5175',
-    ],
+    origin: ['*',
     credentials: true,
   });
 
